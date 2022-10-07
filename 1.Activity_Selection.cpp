6eba53f49a89483f064f3ead2730687462c1fac6
bsen20 <officialbsen@gmail.com>
2022-10-07 23:53:20 +0530

    Greedy problem added

---
+++ 1.Activity_Selection.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    static bool comp(const pair<int, int>&a, const pair<int, int>&b){
+            if(a.second == b.second)
+                return a.first  <= b.first;
+            return a.second < b.second;
+        }
+    
+        int maxMeetings(int start[], int end[], int n) {
+        // Your code here
+        vector<pair<int, int>>vec;
+        int i,c=0, fin;
+        for(i=0;i<n;i++){
+            vec.push_back({start[i], end[i]});
+        }
+        sort(vec.begin(), vec.end(), comp);
+        
+        for(i=0;i<n;){
+            c++;
+            fin=vec[i].second;
+            i++;
+            while(i < n && fin >= vec[i].first){
+                i++;
+            }
+        }
+        return c;
+        // Your code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
